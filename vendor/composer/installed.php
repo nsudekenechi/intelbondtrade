31368b99fb0dea8083bab8c1fd940c7c28607697
kenechi <nsudekenechi2@gmail.com>
2024-04-27 22:34:03 +0100

    making changes

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => '__root__',
-        'pretty_version' => 'dev-master',
-        'version' => 'dev-master',
-        'reference' => 'f9c8e667c59d9342f19754665295bf495cdb8a61',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '57ccb40b51adbbe298176db2d61425ff8dae5776',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         '__root__' => array(
-            'pretty_version' => 'dev-master',
-            'version' => 'dev-master',
-            'reference' => 'f9c8e667c59d9342f19754665295bf495cdb8a61',
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '57ccb40b51adbbe298176db2d61425ff8dae5776',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
